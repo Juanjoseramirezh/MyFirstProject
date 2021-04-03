f8cd7855e76d336e2da6af0fffaa7eba491e7df3
Juan Jose Ramirez <juanjose.rami25@gmail.com>
2021-04-03 13:23:14 -0300

    :smile: this is the project

---
+++ MyOwnProgram.java
@@ -0,0 +1,12 @@
+public class MyOwnProgram {
+    public static void main(String[] args) {
+        String name = "Juan Jose";
+        int age = 27;
+        String city = "Santiago";
+        String country = "Chile";
+        System.out.println("Mi nombre es " + name);
+        System.out.println("Tengo " + age + " años de edad");
+        System.out.println("Vivo en " + city + " de " + country);
+
+    }
+}
